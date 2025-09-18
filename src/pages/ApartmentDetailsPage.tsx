c979e214fbcfad1378f1c88b296c74af076ab14e
DenislavVasilev <142880706+DenislavVasilev@users.noreply.github.com>
2025-09-18 20:11:02 +0300

    Remove phone icon from contact button

--- src/pages/ApartmentDetailsPage.tsx
+++ src/pages/ApartmentDetailsPage.tsx
@@ -344,7 +344,6 @@ const ApartmentDetailsPage = () => {
                         }
                       }}
                     >
-                      <Phone className="w-5 h-5 mr-2 md:hidden" />
                       Свържете се с нас
                     </button>
                   </div>
